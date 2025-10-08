f01235f50d43701290d1c258cb8b7bb05e31287e
Lacroix-Eva <eva.lacroix@edu.univ-fcomte.fr>
2025-10-08 16:19:23 +0200

    colle3

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -3,6 +3,6 @@ import type { TypedPocketBase } from "./pocketbase-types";
 var path='';
 if(import.meta.env.MODE === 'development')
     path = 'http://localhost:8090'    //localhost = machine de dev
-else path = 'http://localhost:7070'   //localhost = machine de déploiement
+else path = 'http://localhost:3070'   //localhost = machine de déploiement
 const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
